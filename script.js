e792d9d6b84d1d1e9910630e63cedb03afc37dae
jptrom <jptromz@gmail.com>
2025-04-30 02:17:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const botao = document.getElementById('botao');
+const mensagemDiv = document.getElementById('mensagem');
+
+botao.addEventListener('click', function() {
+    mensagemDiv.textContent = "404 - Pessoa ruim não encontrada. Só há gente boa aqui!";
+});
+
+
+
+
+
+
